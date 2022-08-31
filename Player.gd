dda2dcef66950e3bd3cf4dfbd0ff6433d5233628
maceliem <mattias@kirkaune.com>
2022-08-31 13:45:30 +0200

    Fancy road and walls

--- Player.gd
+++ Player.gd
@@ -1,12 +1,27 @@
 extends VehicleBody
 
 var maxRPM := 3000
-var maxTorque := 400
+var maxTorque := 800
+var offroad := 0.5
+
+func _ready():
+	contact_monitor = true
+	contacts_reported = 1000
 
 func _physics_process(delta):
-    steering = lerp(steering, Input.get_axis("right", "left") * 0.05, 10 * delta)
-    var acceleration = Input.get_axis("break", "accelerate")
-    var rpm = $back_left.get_rpm()
-    $back_left.engine_force = acceleration * maxTorque * (1 - rpm / maxRPM)
-    rpm = $back_right.get_rpm()
-    $back_right.engine_force = acceleration * maxTorque * (1 - rpm / maxRPM)
+	steering = lerp(steering, Input.get_axis("right", "left") * 0.05, 10 * delta)
+	var acceleration = Input.get_axis("break", "accelerate")
+	var rpm = $back_left.get_rpm()
+	var curTorque := maxTorque
+	var curMaxRPM := maxRPM
+	var onRoad:= false
+	for obj in $Area.get_overlapping_bodies():
+		if obj.name == "Road":
+			onRoad = true
+	if !onRoad: 
+		curTorque *= offroad
+		curMaxRPM *= offroad
+	print(curTorque)
+	$back_left.engine_force = acceleration * curTorque * (1 - rpm / curMaxRPM)
+	rpm = $back_right.get_rpm()
+	$back_right.engine_force = acceleration * curTorque * (1 - rpm / curMaxRPM)
---
+++ manualTrack.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name Track
+
+func _ready():
+	$Road.translation.y = 0
